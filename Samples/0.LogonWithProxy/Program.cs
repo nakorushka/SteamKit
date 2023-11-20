09fd873f31479f88a8f28df735e2cd3b18ac91f1
Dmitry Danilin <dmitrydnl99@gmail.com>
2023-11-20 22:52:08 +0300

    for connect to steam

--- Samples/0.LogonWithProxy/Program.cs
+++ Samples/0.LogonWithProxy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using SteamKit2;
 
 namespace Sample0_LogonWithProxy
 {
@@ -9,8 +10,44 @@ namespace Sample0_LogonWithProxy
         static void Main( string[] args )
         {
             var logonData = ReadLogonData();
-            Console.WriteLine( logonData.Username + " " + logonData.Password );
-            Console.ReadKey();
+            string previouslyStoredGuardData = null;
+
+            if ( string.IsNullOrEmpty( logonData.Username ) || string.IsNullOrEmpty( logonData.Password ) )
+            {
+                Console.Error.WriteLine( "Username or password is empty!" );
+                return;
+            }
+
+            // create our steamclient instance
+            var steamClient = new SteamClient();
+            // create the callback manager which will route callbacks to function calls
+            var manager = new CallbackManager( steamClient );
+
+            // get the steamuser handler, which is used for logging on after successfully connecting
+            var steamUser = steamClient.GetHandler<SteamUser>();
+
+            // register a few callbacks we're interested in
+            // these are registered upon creation to a callback manager, which will then route the callbacks
+            // to the functions specified
+            manager.Subscribe<SteamClient.ConnectedCallback>( OnConnected );
+            manager.Subscribe<SteamClient.DisconnectedCallback>( OnDisconnected );
+
+            manager.Subscribe<SteamUser.LoggedOnCallback>( OnLoggedOn );
+            manager.Subscribe<SteamUser.LoggedOffCallback>( OnLoggedOff );
+
+            var isRunning = true;
+
+            Console.WriteLine( "Connecting to Steam..." );
+
+            // initiate the connection
+            steamClient.Connect();
+
+            // create our callback handling loop
+            while ( isRunning )
+            {
+                // in order for the callbacks to get routed, they need to be handled by the manager
+                manager.RunWaitCallbacks( TimeSpan.FromSeconds( 1 ) );
+            }
         }
 
         private static LogonData ReadLogonData()
