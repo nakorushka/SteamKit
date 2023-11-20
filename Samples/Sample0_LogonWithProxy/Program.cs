622660e3a2ce5fe22a551bf27bb9ab2ce9bf2cee
Dmitry Danilin <dmitrydnl99@gmail.com>
2023-11-20 22:39:16 +0300

    read logon data

--- Samples/Sample0_LogonWithProxy/Program.cs
+++ Samples/Sample0_LogonWithProxy/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿using Newtonsoft.Json;
+
+namespace Sample0_LogonWithProxy
+{
+    public class Program
+    {
+        private const string LogonDataPath = "logon_data.json";
+
+        static void Main( string[] args )
+        {
+            var logonData = ReadLogonData();
+            Console.WriteLine( logonData.Username + " " + logonData.Password );
+            Console.ReadKey();
+        }
+
+        private static LogonData ReadLogonData()
+        {
+            using StreamReader reader = new( LogonDataPath );
+            var json = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<LogonData>( json );
+        }
+    }
+}
