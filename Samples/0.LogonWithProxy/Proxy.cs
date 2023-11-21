5147ae72fe03e911facaff4ecb17349db6759b07
Dmitry Danilin <dmitrydnl99@gmail.com>
2023-11-21 22:30:36 +0300

    Create Proxy.cs

---
+++ Samples/0.LogonWithProxy/Proxy.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace Sample0_LogonWithProxy
+{
+    public static class Proxy
+    {
+        public static IWebProxy Get( ProxyData proxyData )
+        {
+            return new WebProxy( proxyData.Address, proxyData.Port )
+            {
+                Credentials = new NetworkCredential( proxyData.Username, proxyData.Password )
+            };
+        }
+
+    }
+}
