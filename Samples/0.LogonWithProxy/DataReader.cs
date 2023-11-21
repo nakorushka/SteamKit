b58e58c4270bb85a21fcf538c5a5ac4c6470768a
Dmitry Danilin <dmitrydnl99@gmail.com>
2023-11-21 22:27:11 +0300

    Create DataReader.cs

---
+++ Samples/0.LogonWithProxy/DataReader.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace Sample0_LogonWithProxy
+{
+    public static class DataReader
+    {
+        const string LogonDataPath = "logon_data.json";
+        const string ProxyDataPath = "proxy_data.json";
+
+        public static LogonData ReadLogonData()
+        {
+            using StreamReader reader = new( LogonDataPath );
+            var json = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<LogonData>( json );
+        }
+
+        public static ProxyData ReadProxyData()
+        {
+            using StreamReader reader = new( ProxyDataPath );
+            var json = reader.ReadToEnd();
+            return JsonConvert.DeserializeObject<ProxyData>( json );
+        }
+    }
+}
