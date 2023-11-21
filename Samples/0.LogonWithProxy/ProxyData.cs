b54e485fa33ecbd76aa185a1a5ea766cb5dedfb2
Dmitry Danilin <dmitrydnl99@gmail.com>
2023-11-21 22:23:45 +0300

    Create ProxyData.cs

---
+++ Samples/0.LogonWithProxy/ProxyData.cs
@@ -0,0 +1,10 @@
+﻿namespace Sample0_LogonWithProxy
+{
+    public class ProxyData
+    {
+        public string Address { get; set; } = string.Empty;
+        public int Port { get; set; } = 0;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
